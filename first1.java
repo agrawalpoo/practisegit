74814a7db66766dcf1da261377ffaf3763f10f16
meso <meso@gmail.com>
2022-12-01 12:23:37 +0530

    first1 added

---
+++ first1.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+class first
+{
+public static void main(String ar[]){
+String name;
+Scanner sc=new Scanner(System.in);
+
+System.out.println("may i know your name");
+name=sc.nextLine();
+System.out.println("welcome "+name);
+}
+}
