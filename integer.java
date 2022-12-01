8905a69aa1b909e3648bde1474b8e1cac0f8c24a
meso <meso@gmail.com>
2022-12-01 14:22:31 +0530

    to check range of integer udemy

---
+++ integer.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+class integer{
+public static void main(String arg[]){
+System.out.println("min int value is:" + Integer.MIN_VALUE);
+System.out.println("max int value is:" + Integer.MAX_VALUE);
+System.out.println("int bytes:" + Integer.BYTES);
+}
+}
